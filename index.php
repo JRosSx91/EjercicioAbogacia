5b64851159ef184124a1bd00cc68d58b538fcc4d
JRosSx91 <hazee.1491@gmail.com>
2023-05-05 13:05:56 +0200

    add php pages

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cita previa - Ilustre Colegio de Abogados de Antoni Algueró</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/estils.css">
+	</head>
+	<body>
+		<main>
+			<header>
+				<div id="cabecera1">
+					
+				</div>
+				<div id="cabecera2">
+					<hgroup>
+						<h2>Justicia Gratuita - Cita Previa</h2>
+						<h4>Servicio de Orientación Jurídica</h4>
+					</hgroup>
+				</div>
+			</header>
+			<aside>
+				<section id="lateral">
+						<ul>
+							<li class="first"><a href="#">Cita Previa</a></li>
+							<li><a href="#">Normativa</a></li>
+						</ul>
+				</section>
+			</aside>
+			<div id="principal">
+				<section id="formulario">
+					<div class="consulta1">
+						<h2>Cita Previa - Primera consulta</h2>
+						<p>Paso 1: Seleccione la delegación del Ilustre Colegio de Abogados donde prefiere ser atendido:</p>
+						<div class="opciones">
+							<div class="op1">
+								<h1>BARCELONA</h1>
+								<h4>Avinguda Diagonal 447 - 08006<br>T: 93 201 00 10</h4>
+							</div>
+							<div class="op2">
+								<h1>SANT JUST DESVERN</h1>
+								<h4>Carrer Parlament Català 1 - 08960<br>T: 93 473 89 84</h4>
+							</div>
+							<div class="mapa">
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2993.819323467304!2d2.0730444512508637!3d41.378010004437144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a49979f301c01b%3A0xcd0b566b421b5467!2sEscola+de+Formaci%C3%B3+%22Antoni+Alguer%C3%B3%22!5e0!3m2!1ses!2ses!4v1496058886022" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section id="resultado">
+					<div></div>
+				</section>
+			</div>
+		</main>
+		<footer>
+			<div id="footer">
+				<p>Hola</p>
+			</div>
+		</footer>
+	</body>
+</html>
